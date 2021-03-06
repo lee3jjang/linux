49bbe87727717f74ee8ec8c8bc992f81dcf8ce23
Sang-Jin <lee3jjang@gmail.com>
2021-03-07 00:03:11 +0900

    gcc

--- ex.sh
+++ ex.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
 
-user=$(whoami)
-input=/home/$user
-
-total_files () {
-    find $1 -type f | wc -l
-}
-
-total_folders () {
-    find $1 -type d | wc -l
-}
-
-echo `total_files $input`
-echo `total_folders $input`
-echo $0
-echo $#
-echo $@
-echo $$
-
-val=`expr 2 + 35 - 3 \* 3`
-echo $val
